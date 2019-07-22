853ae588319440bd84abf46161eb58965bf213da
François <glavieux.francois@orange.fr>
2019-07-22 13:50:24 +0200

    MAJ

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function calcul(){
+  var number = document.getElementById('number').value;
+  window.alert(number * 10);
+}
+
+/*
+function emptyField(){
+  document.getElementById('number').value = "";
+}
+*/
+
+var number = document.querySelector('input').value;
+alert(number);
